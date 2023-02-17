31381c529dcd6673107893798d595096be08527a
Myungkyulee <skdlaudrb@naver.com>
2023-02-17 13:32:48 +0900

    feat : implement List and test

--- study.structure.data/src/List.java
+++ study.structure.data/src/List.java
@@ -7,20 +7,154 @@
  * ....
  */
 
-public class List {
+public class List<T> {
     // 여기에 구현
-    public void testMyCode () {
-        /*
-        * 필수 작성
-        * 데이터 생성, 조회, 변경, 삭제코드를 작성합니다.
-        * */
+    private Node<T> start;
+    private Node<T> end;
+    private int size;
 
-        // 데이터 생성
-
-        // 데이터 조회
-
-        // 데이터 변경
-
-        // 데이터 삭제
+    public List() {
+        size = 0;
     }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(T value) {
+        Node<T> addedNode = new Node<>(value);
+        if (isEmpty()) {
+            start = end = addedNode;
+        } else {
+            // 새로운 노드를 연결
+            addedNode.setFront(end);
+            end.setRear(addedNode);
+
+            end = addedNode;
+        }
+        size++;
+    }
+
+    public T get(int index) {
+        Node<T> currentNode;
+
+        int count = 0;
+        for (currentNode = start; currentNode != null; currentNode = currentNode.getRear(), count++) {
+            if (count == index) {
+                return currentNode.getValue();
+            }
+        }
+        throw new ArrayIndexOutOfBoundsException();
+    }
+
+    public T remove(int index) {
+        Node<T> currentNode = start;
+        int count = 0;
+        for (currentNode = start; currentNode != null; currentNode = currentNode.getRear(), count++) {
+            if (count == index) {
+                Node<T> front = currentNode.getFront();
+                Node<T> rear = currentNode.getRear();
+
+                // 삭제되는 앞뒤의 노드 연결하기
+                front.setRear(rear);
+                rear.setFront(front);
+
+                // 삭제되는 노드 연결 끊기
+                currentNode.setFront(null);
+                currentNode.setRear(null);
+
+                size--;
+                // 삭제되는 노드 value반환
+                return currentNode.getValue();
+            }
+        }
+        throw new ArrayIndexOutOfBoundsException();
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            // null을 반환하는 게 좋은지 예외를 던지는게 좋은지 모르겠습니다.
+            return null;
+        }
+        Node<T> removedNode = start;
+        start = removedNode.getRear();
+
+        //연결을 끊는 작업
+        start.setFront(null);
+        removedNode.setRear(null);
+        size--;
+        return removedNode.getValue();
+    }
+
+    public T removeLast() {
+        if (isEmpty()) {
+            // null을 반환하는 게 좋은지 예외를 던지는게 좋은지 모르겠습니다.
+            return null;
+        }
+        Node<T> removedNode = end;
+        end = removedNode.getFront();
+
+        // 연결을 끊는 작업
+        end.setRear(null);
+        removedNode.setFront(null);
+        size--;
+        return removedNode.getValue();
+    }
+
+
+    public void testMyCode() {
+        /**
+         * list내부에서 테스트를 하는 것이 너무 어려워서 Main에서 테스트를 했습니다.
+         */
+    }
+
+
+    private static void printList(List<Integer> myList) {
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.print(myList.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    static class Node<T> {
+        private Node<T> front;
+        private Node<T> rear;
+        private T value;
+
+        public Node() {
+        }
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public Node<T> getFront() {
+            return front;
+        }
+
+        public void setFront(Node<T> front) {
+            this.front = front;
+        }
+
+        public Node<T> getRear() {
+            return rear;
+        }
+
+        public void setRear(Node<T> rear) {
+            this.rear = rear;
+        }
+    }
+
 }
--- study.structure.data/src/Main.java
+++ study.structure.data/src/Main.java
@@ -1,8 +1,83 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        List<Integer> intList = new List<>();
+        List<String> stringList = new List<>();
+        List<Double> doubleList = new List<>();
 
-        List myList = new List();
-        myList.testMyCode();
+        testIntegerList(intList);
+        testStringList(stringList);
+        testDoubleList(doubleList);
+    }
+
+    private static void testDoubleList(List<Double> doubleList) {
+        setListDouble(doubleList);
+        printList(doubleList);
+
+        doubleList.removeFirst();
+        printList(doubleList);
+
+        doubleList.removeLast();
+        printList(doubleList);
+
+        doubleList.remove(1);
+        printList(doubleList);
+    }
+
+    private static void testStringList(List<String> stringList) {
+        setListString(stringList);
+        printList(stringList);
+
+        stringList.removeFirst();
+        printList(stringList);
+
+        stringList.removeLast();
+        printList(stringList);
+
+        stringList.remove(1);
+        printList(stringList);
+    }
+
+    private static void testIntegerList(List<Integer> intList) {
+        setListInteger(intList);
+        printList(intList);
+
+        intList.removeFirst();
+        printList(intList);
+
+        intList.removeLast();
+        printList(intList);
+
+        intList.remove(1);
+        printList(intList);
+    }
+
+    private static void setListInteger(List<Integer> myList) {
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+    }
+    private static void setListString(List<String> myList) {
+        myList.add("홍길동");
+        myList.add("임꺽정");
+        myList.add("안중근");
+        myList.add("이순신");
+        myList.add("장보고");
+    }
+
+    private static void setListDouble(List<Double> myList) {
+        myList.add(1d);
+        myList.add(2.4);
+        myList.add(5.6);
+        myList.add(3.35);
+        myList.add(10D);
+    }
+
+    private static void printList(List<?> myList) {
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.print(myList.get(i) + " ");
+        }
+        System.out.println();
     }
 }
