fe28ee28cfa34859dbe322d15ddff98488556bb2
hanchanhee <hanssang6301@gamil.com>
2023-02-21 21:18:44 +0900

    my_name

--- study.structure.data/src/Chanhee22.java
+++ study.structure.data/src/Chanhee22.java
@@ -2,5 +2,6 @@ public class Chanhee22 {
     public static void main(String[] args) {
         System.out.println("Hello guys,am chahee");
         System.out.println("second");
+        System.out.println("3");
     }
 }
